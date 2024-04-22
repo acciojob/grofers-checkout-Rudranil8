770af27f15cc9e1e01a667cda20c64dcfd21f799
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-22 22:18:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+	let prices = document.querySelectorAll('.prices');
+	let total = 0;
+	for(let i=0; i<prices.length; i++){
+		total+=parseFloat(prices[i].innerText);
+	}
 };
 
 getSumBtn.addEventListener("click", getSum);
